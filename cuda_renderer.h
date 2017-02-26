ac3e64375d6b96273914fdd02b6297e53f1d6d94
Y0Q <Y0Q@users.noreply.github.com>
2017-02-25 23:01:25 -0800

    Add files via upload

---
+++ cuda_renderer.h
@@ -0,0 +1,39 @@
+
+
+#include "render_circle.h"
+
+class Cuda_renderer : public render_circle{
+	
+private:
+
+    Image* image;
+    SceneName sceneName;
+
+    int numCircles;
+    float* position;
+    float* velocity;
+    float* color;
+    float* radius;
+
+    float* cudaDevicePosition;
+    float* cudaDeviceVelocity;
+    float* cudaDeviceColor;
+    float* cudaDeviceRadius;
+    float* cudaDeviceImageData;
+
+public:
+		Cuda_renderer();
+		
+		void image_setup();
+		
+		void allocOutputImage(int width, int height);
+		
+		void clearImage();
+		
+		void render();
+		
+		void pixel_shader( int Index,
+						   float pixelx, float pixely,
+						   float p_x,float p_y, float p_z,
+						   float* pixelData);
+}
---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <getopt.h>
+#include <string>
+
+#include "cuda_renderer.h"
+
+int main(int argc, char** argv)
+{
+	int benchMarkStart = -1;
+	int benchMarkEnd  = -1;
+	int imageSize = 768;
+	
+	std::string sceneNameStr;
+	SceneName sceneName;
+	
+	if (optind >= argc) {
+        fprintf(stderr, "Error: missing scene name\n");
+        usage(argv[0]);
+        return 1;
+    }
+	
+	sceneNameStr = argv[optind];
+	
+	if(sceneNameStr.compare("snow") == 0){
+		sceneName = SNOWFLAKES;
+	} else if (sceneNameStr.compare("rgb") == 0) {
+		sceneName = CIRCLE_RGB;
+	} else {
+		fprintf(stderr,"Unknown Scene Name (%s) \n",sceneNameStr.c_str());
+		return 1;
+	}
+	
+	printf("Rendering to %dx%d image\n", imageSize, imageSize);
+	
+
+	render_circle* cuda_render;
+	
+	cuda_render = new cuda_renderer();
+	
+	cuda_render->allocImageBuf(imageSize, imageSize);
+    cuda_render->loadScene(sceneName);
+    cuda_render->setup();
+	
+	return 0;
+}
